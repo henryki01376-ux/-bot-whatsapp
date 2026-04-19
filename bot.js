89379ec37b5022b9e7f503c86869a2c259c7c781
henryki01376-ux <henryki01376@gmail.com>
2026-04-18 23:53:40 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,32 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Bot conectado!');
+});
+
+client.on('message', async msg => {
+    const chat = await msg.getChat();
+
+    if (!chat.isGroup) return;
+
+    if (msg.body === '!fechar') {
+        await chat.setMessagesAdminsOnly(true);
+        msg.reply('🔒 Grupo fechado');
+    }
+
+    if (msg.body === '!abrir') {
+        await chat.setMessagesAdminsOnly(false);
+        msg.reply('✅ Grupo aberto');
+    }
+});
+
+client.initialize();
